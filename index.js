4cbececcf561da0082da1502821bcdccd2942b08
Raquel <raquel.chacim@hotmail.com>
2022-11-05 21:26:49 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerText = "Raquel is the champion!";
+document.body.appendChild(newHeader);
